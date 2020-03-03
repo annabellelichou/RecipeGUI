3bca7144b61e543a22adc26ebc3671145a57bfab
annabellelichou <61762673+annabellelichou@users.noreply.github.com>
2020-03-04 00:26:22 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,8 +4,11 @@ data_file='recettes.sqlite'
 ma_connexion=sqlite3.connect(data_file)
 mon_curseur=ma_connexion.cursor()
 
+global id_utilisateur
+
 from functions import *
 from gui import *
+
 
 
 page_connexion()
